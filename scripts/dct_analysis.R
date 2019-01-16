f5c4a9ac52d24772a72662cece8c2d999d6609f3
Ronit Jain <jain.ronit@gmail.com>
2019-01-16 13:42:58 -0800

    Genes were replaced

--- scripts/dct_analysis.R
+++ scripts/dct_analysis.R
@@ -25,76 +25,75 @@ dCt<-cSplit(dCt,"Sample", sep= "_", drop=F)
 dCt<-rename(dCt,replace=c("Sample_1"="Ploidy","Sample_2"="Desiccation","Sample_3"="HeatShock","Sample_4"="Sample"))
 
 #calculate normalized expression of target gene Ct relative to actin Ct using: 2^-(delta Ct)
-dCt$CARM<-2^-(dCt$CarmCt-dCt$Actinct)
-dCt$TLR<-2^-(dCt$TLR-dCt$Actinct)
-dCt$TRAF<-2^-(dCt$TRAFct-dCt$Actinct)
-dCt$H2AV<-2^-(dCt$H2AVct-dCt$Actinct)
-dCt$PGRP<-2^-(dCt$PGRP-dCt$Actinct)
-dCt$HSP70<-2^-(dCt$HSP70Ct-dCt$Actinct)
-dCt$BMP2<-2^-(dCt$BMP2-dCt$Actinct)
-dCt$GRB2<-2^-(dCt$GRB2-dCt$Actinct)
-dCt$PGEEP4<-2^-(dCt$PGEEP4ct-dCt$Actinct)
+dCt$HSC70<-2^-(dCt$HSC70-dCt$Actin)
+dCt$DNMT1<-2^-(dCt$DNMT1-dCt$Actin)
+dCt$MBD2<-2^-(dCt$MBD2-dCt$Actin)
+dCt$MeCP2<-2^-(dCt$MeCP2-dCt$Actin)
+dCt$HIF1A<-2^-(dCt$HIF1A-dCt$Actin)
+dCt$HATHaP2<-2^-(dCt$HATHaP2-dCt$Actin)
+dCt$HAT<-2^-(dCt$HAT-dCt$Actin)
+dCt$HSP90<-2^-(dCt$HSP90-dCt$Actin)
+
 
 #log transform the data to develop normality in data
-dCt$CARMlog<-log(dCt$CARM)
-dCt$TLRlog<-log(dCt$TLR)
-dCt$H2AVlog<-log(dCt$H2AV)
-dCt$PGRPlog<-log(dCt$PGRP)
-dCt$HSP70log<-log(dCt$HSP70)
-dCt$BMP2log<-log(dCt$BMP2)
-dCt$GRB2log<-log(dCt$GRB2)
-dCt$PGEEP4log<-log(dCt$PGEEP4)
-dCt$TRAFlog<-log(dCt$TRAF)
+dCt$HSC70log<-log(dCt$HSC70)
+dCt$DNMT1log<-log(dCt$DNMT1)
+dCt$MeCP2log<-log(dCt$MeCP2)
+dCt$HIF1Alog<-log(dCt$HIF1A)
+dCt$HATHaP2log<-log(dCt$HATHaP2)
+dCt$HATlog<-log(dCt$HAT)
+dCt$HSP90log<-log(dCt$HSP90)
+dCt$MBD2log<-log(dCt$MBD2)
 
 #Run ANOVA's on all log transformed data as well as Tukey's Honestly Significant Difference post hoc test
-CARM<-aov(CARMlog~Pop+Treat+Pop:Treat, data=dCt)
-CARM
-TukeyHSD(CARM)
-summary(CARM)
+HSC70<-aov(HSC70log~Pop+Treat+Pop:Treat, data=dCt)
+HSC70
+TukeyHSD(HSC70)
+summary(HSC70)
 
-TLR<-aov(TLRlog~Pop+Treat+Pop:Treat, data=dCt)
-TLR
-TukeyHSD(TLR)
-summary(TLR)
+DNMT1<-aov(DNMT1log~Pop+Treat+Pop:Treat, data=dCt)
+DNMT1
+TukeyHSD(DNMT1)
+summary(DNMT1)
 
-H2AV<-aov(H2AVlog~Pop+Treat+Pop:Treat, data=dCt)
-H2AV
-TukeyHSD(H2AV)
-summary(H2AV)
+MeCP2<-aov(MeCP2log~Pop+Treat+Pop:Treat, data=dCt)
+MeCP2
+TukeyHSD(MeCP2)
+summary(MeCP2)
 
-PGRP<-aov(PGRPlog~Pop+Treat+Pop:Treat, data=dCt)
-PGRP
-TukeyHSD(PGRP)
-summary(PGRP)
+HIF1A<-aov(HIF1Alog~Pop+Treat+Pop:Treat, data=dCt)
+HIF1A
+TukeyHSD(HIF1A)
+summary(HIF1A)
 
-HSP70<-aov(HSP70log~Pop+Treat+Pop:Treat, data=dCt)
-HSP70
-TukeyHSD(HSP70)
-summary(HSP70)
+HATHaP2<-aov(HATHaP2log~Pop+Treat+Pop:Treat, data=dCt)
+HATHaP2
+TukeyHSD(HATHaP2)
+summary(HATHaP2)
 
-BMP2<-aov(BMP2log~Pop+Treat+Pop:Treat, data=dCt)
-BMP2
-TukeyHSD(BMP2)
-summary(BMP2)
+HAT<-aov(HATlog~Pop+Treat+Pop:Treat, data=dCt)
+HAT
+TukeyHSD(HAT)
+summary(HAT)
 
-GRB2<-aov(GRB2log~Pop+Treat+Pop:Treat, data=dCt)
-GRB2
-TukeyHSD(GRB2)
-summary(GRB2)
+HSP90<-aov(HSP90log~Pop+Treat+Pop:Treat, data=dCt)
+HSP90
+TukeyHSD(HSP90)
+summary(HSP90)
 
 PGEEP4<-aov(PGEEP4log~Pop+Treat+Pop:Treat, data=dCt)
 PGEEP4
 TukeyHSD(PGEEP4)
 summary(PGEEP4)
 
-TRAF3<-aov(TRAFlog~Pop+Treat+Pop:Treat, data=dCt)
-TRAF3
-TukeyHSD(TRAF3)
-summary(TRAF3)
+MBD23<-aov(MBD2log~Pop+Treat+Pop:Treat, data=dCt)
+MBD23
+TukeyHSD(MBD23)
+summary(MBD23)
 
 #graph all normalized Ct values to produce boxplots to visualize data
 
-ggplot(data=dCt)+geom_boxplot(aes(x=Treat, y=CARM,fill=Pop))+theme_bw()+
+ggplot(data=dCt)+geom_boxplot(aes(x=Treat, y=HSC70,fill=Pop))+theme_bw()+
   scale_fill_grey(start=0.37, end=.9,
                   labels=c("Dabob Bay","Fidalgo Bay","Oyster Bay"))+
   guides(fill=guide_legend(title="Population"))+
@@ -104,10 +103,10 @@ ggplot(data=dCt)+geom_boxplot(aes(x=Treat, y=CARM,fill=Pop))+theme_bw()+
         legend.key=element_rect(fill=NA))+
   ylim(c(0,0.3))+scale_x_discrete(labels=c("Control","Mechanical","Temperature"))+
   annotate("text",x=c("C","M","T"), y=0.3, label=c("A", "A", "B"), size=10)+
-  labs(x="Treatment", y=expression(paste("CARM Expression (",Delta,"Ct)")))
+  labs(x="Treatment", y=expression(paste("HSC70 Expression (",Delta,"Ct)")))
 
 
-ggplot(data=dCt)+geom_boxplot(aes(x=Treat, y=TLR, fill=Pop))+theme_bw()+
+ggplot(data=dCt)+geom_boxplot(aes(x=Treat, y=DNMT1, fill=Pop))+theme_bw()+
   scale_fill_grey(start=0.37, end=.9,
                   labels=c("Dabob Bay","Fidalgo Bay","Oyster Bay"))+
   guides(fill=guide_legend(title="Population"))+
@@ -116,9 +115,9 @@ ggplot(data=dCt)+geom_boxplot(aes(x=Treat, y=TLR, fill=Pop))+theme_bw()+
         legend.position=c(.09,.87),panel.grid.major=element_blank(),
         legend.key=element_rect(fill=NA))+
   ylim(c(0,0.005))+scale_x_discrete(labels=c("Control","Mechanical","Temperature"))+
-  labs(x="Treatment", y=expression(paste("TLR Expression (",Delta,"Ct)")))
+  labs(x="Treatment", y=expression(paste("DNMT1 Expression (",Delta,"Ct)")))
 
-ggplot(data=dCt)+geom_boxplot(aes(x=Treat, y=H2AV,fill=Pop))+theme_bw()+
+ggplot(data=dCt)+geom_boxplot(aes(x=Treat, y=MeCP2,fill=Pop))+theme_bw()+
   scale_fill_grey(start=0.37, end=.9,
                   labels=c("Dabob Bay","Fidalgo Bay","Oyster Bay"))+
   guides(fill=guide_legend(title="Population"))+
@@ -129,9 +128,9 @@ ggplot(data=dCt)+geom_boxplot(aes(x=Treat, y=H2AV,fill=Pop))+theme_bw()+
   ylim(c(0,1.9))+scale_x_discrete(labels=c("Control","Mechanical","Temperature"))+
   annotate("text",x=c("C","M","T"), y=1.9, label=c("A", "A", "B"), size=10)+
   annotate("text",x=c(1.25,3.25), y=1.85, label=c("*","*"), size=12)+
-  labs(x="Treatment", y=expression(paste("H2AV Expression (",Delta,"Ct)")))
+  labs(x="Treatment", y=expression(paste("MeCP2 Expression (",Delta,"Ct)")))
 
-ggplot(data=dCt)+geom_boxplot(aes(x=Treat, y=PGRP,fill=Pop))+theme_bw()+
+ggplot(data=dCt)+geom_boxplot(aes(x=Treat, y=HIF1A,fill=Pop))+theme_bw()+
   scale_fill_grey(start=0.37, end=.9,
                   labels=c("Dabob Bay","Fidalgo Bay","Oyster Bay"))+
   guides(fill=guide_legend(title="Population"))+
@@ -140,9 +139,9 @@ ggplot(data=dCt)+geom_boxplot(aes(x=Treat, y=PGRP,fill=Pop))+theme_bw()+
         legend.position=c(.09,.87),panel.grid.major=element_blank(),
         legend.key=element_rect(fill=NA))+
   ylim(c(0,0.005))+scale_x_discrete(labels=c("Control","Mechanical","Temperature"))+
-  labs(x="Treatment", y=expression(paste("PGRP Expression (",Delta,"Ct)")))
+  labs(x="Treatment", y=expression(paste("HIF1A Expression (",Delta,"Ct)")))
 
-ggplot(data=dCt)+geom_boxplot(aes(x=Treat, y=HSP70,fill=Pop))+theme_bw()+
+ggplot(data=dCt)+geom_boxplot(aes(x=Treat, y=HATHaP2,fill=Pop))+theme_bw()+
   scale_fill_grey(start=0.37, end=.9,
                   labels=c("Dabob Bay","Fidalgo Bay","Oyster Bay"))+
   guides(fill=guide_legend(title="Population"))+
@@ -153,9 +152,9 @@ ggplot(data=dCt)+geom_boxplot(aes(x=Treat, y=HSP70,fill=Pop))+theme_bw()+
   ylim(c(0,.5))+scale_x_discrete(labels=c("Control","Mechanical","Temperature"))+
   annotate("text",x=c("C","M","T"), y=.5, label=c("AB", "A", "B"), size=10)+
   annotate("text",x=c(2.25,3.25), y=.4, label=c("*","*"), size=12)+
-  labs(x="Treatment", y=expression(paste("HSP70 Expression (",Delta,"Ct)")))
+  labs(x="Treatment", y=expression(paste("HATHaP2 Expression (",Delta,"Ct)")))
 
-ggplot(data=dCt)+geom_boxplot(aes(x=Treat, y=BMP2,fill=Pop))+theme_bw()+
+ggplot(data=dCt)+geom_boxplot(aes(x=Treat, y=HAT,fill=Pop))+theme_bw()+
   scale_fill_grey(start=0.37, end=.9,
                   labels=c("Dabob Bay","Fidalgo Bay","Oyster Bay"))+
   guides(fill=guide_legend(title="Population"))+
@@ -165,9 +164,9 @@ ggplot(data=dCt)+geom_boxplot(aes(x=Treat, y=BMP2,fill=Pop))+theme_bw()+
         legend.key=element_rect(fill=NA))+
   ylim(c(0,2.5))+scale_x_discrete(labels=c("Control","Mechanical","Temperature"))+
   annotate("text",x=c(1.25,2.25), y=2.5, label=c("*","*"), size=12)+
-  labs(x="Treatment", y=expression(paste("BMP2 Expression (",Delta,"Ct)")))
+  labs(x="Treatment", y=expression(paste("HAT Expression (",Delta,"Ct)")))
 
-ggplot(data=dCt)+geom_boxplot(aes(x=Treat, y=GRB2,fill=Pop))+theme_bw()+
+ggplot(data=dCt)+geom_boxplot(aes(x=Treat, y=HSP90,fill=Pop))+theme_bw()+
   scale_fill_grey(start=0.37, end=.9,
                   labels=c("Dabob Bay","Fidalgo Bay","Oyster Bay"))+
   guides(fill=guide_legend(title="Population"))+
@@ -177,7 +176,7 @@ ggplot(data=dCt)+geom_boxplot(aes(x=Treat, y=GRB2,fill=Pop))+theme_bw()+
         legend.key=element_rect(fill=NA))+
   ylim(c(0,1.5))+scale_x_discrete(labels=c("Control","Mechanical","Temperature"))+
   annotate("text",x=c(1.25,2.25), y=1.27, label=c("*","*"), size=12)+
-  labs(x="Treatment", y=expression(paste("GRB2 Expression (",Delta,"Ct)")))
+  labs(x="Treatment", y=expression(paste("HSP90 Expression (",Delta,"Ct)")))
 
 ggplot(data=dCt)+geom_boxplot(aes(x=Treat, y=PGEEP4,fill=Pop))+theme_bw()+
   scale_fill_grey(start=0.37, end=.9,
@@ -190,7 +189,7 @@ ggplot(data=dCt)+geom_boxplot(aes(x=Treat, y=PGEEP4,fill=Pop))+theme_bw()+
   ylim(c(0,0.15))+scale_x_discrete(labels=c("Control","Mechanical","Temperature"))+
   labs(x="Treatment", y=expression(paste("PGEEP4 Expression (",Delta,"Ct)")))
 
-ggplot(data=dCt)+geom_boxplot(aes(x=Treat, y=TRAF,fill=Pop))+theme_bw()+
+ggplot(data=dCt)+geom_boxplot(aes(x=Treat, y=MBD2,fill=Pop))+theme_bw()+
   scale_fill_grey(start=0.37, end=.9,
                   labels=c("Dabob Bay","Fidalgo Bay","Oyster Bay"))+
   guides(fill=guide_legend(title="Population"))+
@@ -200,7 +199,7 @@ ggplot(data=dCt)+geom_boxplot(aes(x=Treat, y=TRAF,fill=Pop))+theme_bw()+
         legend.key=element_rect(fill=NA))+
   ylim(c(0,.3))+scale_x_discrete(labels=c("Control","Mechanical","Temperature"))+
   annotate("text",x=c("C","M","T"), y=.3, label=c("A", "B", "AB"), size=10)+
-  labs(x="Treatment", y=expression(paste("TRAF3 Expression (",Delta,"Ct)")))
+  labs(x="Treatment", y=expression(paste("MBD23 Expression (",Delta,"Ct)")))
 
 
 
